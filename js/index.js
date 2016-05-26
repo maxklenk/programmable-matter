a9c38aabe91c5bb203bab6d6c614bbf368ea7cd8
Henriette Dinger <henriette.dinger@student.hpi.uni-potsdam.de>
2016-05-26 11:44:47 +0200

    Update scale per body (WIP)

--- js/index.js
+++ js/index.js
@@ -8,12 +8,16 @@ function showMenu(point, body) {
   var menu = jQuery('.element-menu');
   //TODO: set properties of menu according to body
   if (bodyPropertiesForBodyId[body.id]) {
+    console.log(bodyPropertiesForBodyId[body.id]);
     jQuery('#angle').val(bodyPropertiesForBodyId[body.id].angle * 180/3.14159);
+    jQuery('#size').val(bodyPropertiesForBodyId[body.id].scale * 1000)
   } else {
     jQuery('#angle').val('0');
+    jQuery('#size').val('1000');
     bodyPropertiesForBodyId[body.id] = {};
     bodyPropertiesForBodyId[body.id].angle = body.angle;
     bodyPropertiesForBodyId[body.id].startAngle = body.angle;
+    bodyPropertiesForBodyId[body.id].scale = 1;
   }
   var canvasContainer = jQuery('#myCanvas');
   var top = point.y + canvasContainer.offset().top - (menu.height())/2;
@@ -23,12 +27,13 @@ function showMenu(point, body) {
 }
 
 function scaleBody(factor) {
-    if (lastScale === undefined) {
+    if (bodyPropertiesForBodyId[bodyFromMenu.id].lastScale === undefined) {
       lastScale = factor;
     }
     var newScale = factor / lastScale;
-    console.log("lastScale: " + lastScale + " newScale: " + newScale);
+    bodyPropertiesForBodyId[bodyFromMenu.id].scale = factor;
     lastScale = factor;
+    bodyPropertiesForBodyId[bodyFromMenu.id].lastScale = factor;
     Matter.Body.scale(bodyFromMenu, newScale, newScale);
 }
 
